f380546e618491f6454d4b0682d23e6a6bf8da6b
David Eloy Saavedra <davideloymartinez@gmail.com>
2024-02-23 20:10:56 -0800

    more allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,13 @@
 const allowedOrigins = [
   "https://www.passwordmanager-test.dev",
   "passwordmanager-test.dev",
+  "passwordmanager-backend.onrender.com",
+  "https://passwordmanager-backend.onrender.com",
+  "https://api.passwordmanager-test.dev",
+
+  "https://www.passwordmanager-test.dev",
+  "https://passwordmanager-frontend.onrender.com",
+  "https://www.passwordmanager-test.dev",
   // "http://127.0.0.1:5173",
   // "http://localhost:5173",
 ];
