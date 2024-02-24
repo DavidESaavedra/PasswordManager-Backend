5e9669f99b33f5af93c98ac2e46c8827f57a7fe1
David Eloy Saavedra <davideloymartinez@gmail.com>
2024-02-23 19:40:14 -0800

    add more origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 // do not leave "/" at the end of links breaks cors
 const allowedOrigins = [
   "https://www.passwordmanager-test.dev",
+  "passwordmanager-test.dev",
   // "http://127.0.0.1:5173",
   // "http://localhost:5173",
 ];
